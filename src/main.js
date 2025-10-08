adb088dbe0921b0b88ef79a91103e71081684af7
Adesh Katiya <adeshkatiya27@gmail.com>
2025-10-08 12:39:45 +0530

    skip to content button and mobile menu functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const navToggleSpanEl = navToggle.querySelector("span");
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggleSpanEl.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggleSpanEl.setAttribute("aria-expanded", false);
+    }
+
+    console.log(navToggle.getAttribute("aria-expanded"));
+});
