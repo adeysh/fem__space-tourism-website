b086667da2d0aeb1b8d2143df272e82c67c67c70
Adesh Katiya <adeshkatiya27@gmail.com>
2025-10-17 00:38:05 +0530

    change vite config to resolve path for different html files in the root directory

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+const pathResolve = (dir) => resolve(__dirname, dir);
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    base: "/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: pathResolve("index.html"),
+                destination: pathResolve("destination.html"),
+                crew: pathResolve("crew.html"),
+                technology: pathResolve("technology.html"),
+            },
+            output: {},
+        },
+        emptyOutDir: true,
+    },
 });
